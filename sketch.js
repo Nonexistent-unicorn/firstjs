757993de6b4370479a2f02dab073e0943089bca8
Olivia Stone <ods20a@fsu.edu>
2022-10-18 00:47:35 -0400

    notes

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
+let w=400;
+let h=400;
+let rectWidth = 0
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(w,h);
+  rectMode(CENTER);
+  
 }
 
 function draw() {
   background(125);
+  fill(100, 20, 10)
+  stroke(225)
+  strokeWeight(5)
+
+  rectWidth++;
+  if (rectWidth > 100) {
+    rectWidth = 0;
+  }
+  rect(w/2, h/2, rectWidth, 57);
+  
+
 }
